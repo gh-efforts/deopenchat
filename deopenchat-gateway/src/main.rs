82ee62d6d8cbb8f5f8f483d11ac32b8b3fe6f9f6
xutianyi <xutianyi1999@live.com>
2024-12-24 13:34:54 +0800

    update

--- deopenchat-gateway/src/main.rs
+++ deopenchat-gateway/src/main.rs
@@ -292,6 +292,12 @@ async fn commit_handler<T, P> (
         info!("claim TX: {}", tx);
 
         ctx.md_cache.commit(&commit_claims).await?;
+
+        let total_token_consumed = commit_claims.into_iter()
+            .map(|c| c.tokens_consumed)
+            .sum();
+
+        ctx.accumulated_tokens.fetch_sub(total_token_consumed, Ordering::Relaxed);
     }
 }
 
